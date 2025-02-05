f04b280cbdb2076f354b4229389249b0aa2b7b06
David Sosa-Trejo <dvdsosa@gmail.com>
2025-02-06 00:44:57 +0100

    feat: finish LUT creation script
    Saves into a sqlite database the LUTs for the
    plankton dataset.

--- 04LUTcreation.py
+++ 04LUTcreation.py
@@ -3,153 +3,104 @@ import os
 import numpy as np
 import sqlite3
 
-import matplotlib.pyplot as plt
-
 import torch.backends.cudnn as cudnn
 from torchvision import datasets, transforms
 from SupContrast.networks.resnet_big import FeatureExtractor
 from SupContrast.networks.resnet_big import SupConResNet, LinearClassifier
-
-from PIL import Image
 from tqdm import tqdm
-from io import BytesIO
-from torch.utils.data import DataLoader
 
-# # Check if CUDA is available and set the device accordingly
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+def store_feature(label, feature_vector, cursor, conn):
+    """
+    Stores a feature vector in the database with the associated label.
 
-# # Instantiate the feature extractor
-# feature_extractor = FeatureExtractor(name='resnet50timm')
+    Args:
+        label (str): The label associated with the feature vector.
+        feature_vector (numpy.ndarray): The feature vector to be stored.
+        cursor (sqlite3.Cursor): The database cursor for executing SQL commands.
+        conn (sqlite3.Connection): The database connection object.
 
-# # Load the pre-trained weights
-# state_load = torch.load('/home/dsosatr/tesis/cnnmodel/SupContrast/save/SupCon/path_models/SupCon_path_resnet50timm_lr_0.016_decay_0.0001_bsz_32_temp_0.07_trial_0_cosine_warm_falta/ckpt_epoch_850.pth')
+    Returns:
+        None
+    """
+    cursor.execute('''
+    INSERT INTO plankton_features (label, vector)
+    VALUES (?, ?)
+    ''', (label, feature_vector.tobytes()))
+    conn.commit()
 
-# ###################################
-# # Filter out the keys for the projection head
-# #state_dict = {k: v for k, v in state_load['model'].items() if 'head' not in k}
+def init_db(conn, cursor):
+    """
+    Initializes the database by creating the 'plankton_features' table if it does not already exist.
 
-# # This code is a more verbose way of the above line
-# # Initialize an empty dictionary to store the filtered state dict
-# filtered_state_dict = {}
+    Args:
+        conn (sqlite3.Connection): The connection object to the SQLite database.
+        cursor (sqlite3.Cursor): The cursor object to execute SQL commands.
 
-# # Get the state dict from the loaded state
-# loaded_state_dict = state_load['model']
+    The 'plankton_features' table has the following columns:
+        - id: INTEGER PRIMARY KEY AUTOINCREMENT
+        - label: TEXT NOT NULL
+        - vector: BLOB NOT NULL
 
-# # Loop over each item in the loaded state dict
-# for key, value in loaded_state_dict.items():
-#     # Check if the key does not contain the string 'head'
-#     if 'head' not in key:
-#         # If the key does not contain 'head', add it to the filtered state dict
-#         filtered_state_dict[key] = value
-#     else:
-#         print(f"Projection head found!: {key}")
+    Commits the transaction after executing the SQL command.
+    """
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS plankton_features (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        label TEXT NOT NULL,
+        vector BLOB NOT NULL
+    )
+    ''')
+    conn.commit()
 
-# # Now, filtered_state_dict contains the same items as the loaded state dict,
-# # but without the items where the key contains 'head'
-# state_dict = filtered_state_dict
-# ###################################
-
-# # Load the pre-trained weights into the feature extractor
-# feature_extractor.load_state_dict(state_dict, strict=False)
-
-# # Set the model in evaluation mode. This is important to prevent the BatchNorm
-# # and Dropout layers from changing their behavior
-# # Tested and not difference in the "behaviour"
-# #feature_extractor.eval()
-
-# # Move the feature extractor to the GPU
-# feature_extractor.to(device)
-
-# # Define the transformation. 
-# transformada = transforms.Compose([
-#     transforms.ToTensor(), # Convert the image to a tensor
-#     transforms.Normalize(mean=[0.0419, 0.0355, 0.0410], std=[0.0959, 0.0913, 0.0771]), # Perform the same normalization as the one used while training
-# ])
-
-# # Connect to the SQLite database
-# conn = sqlite3.connect('trainingSetNew.db') # image_features.db
-# # Create a cursor object
-# c = conn.cursor()
-# # Create table
-# # https://sqlbolt.com/lesson/creating_tables
-# c.execute('''CREATE TABLE IF NOT EXISTS features (
-#           image_id INTEGER PRIMARY KEY,
-#           class TEXT NOT NULL, 
-#           image_name TEXT NOT NULL,
-#           tensor BLOB NOT NULL
-# )''')
-
-# def get_file_info(root_folder, transformada):
-
-#     train_dataset = datasets.ImageFolder(root=root_folder, transform=transformada)
-
-#     # Create a data loader
-#     train_loader = DataLoader(train_dataset, batch_size=1, shuffle=False, pin_memory=True, num_workers=1)
-
-#     for idx, (images, labels) in tqdm(enumerate(train_loader)):
-
-#         # Display the first image in the pair
-#         # plt.figure(figsize=(5, 5))
-#         # plt.subplot(1, 2, 1)
-#         # plt.imshow(images[0].permute(1, 2, 0))
-#         # plt.title(train_dataset.classes[labels[0]])
-
-#         # plt.show()
-
-#         # Move the image to the GPU
-#         images = images.to(device)
-
-#         # Disables gradient calculation for the inference step. This can 
-#         # be particularly useful when you’re working with large models or 
-#         # large inputs, where memory usage could be a concern.
-#         with torch.no_grad():
-#             # Use the feature extractor
-#             feature_vector = feature_extractor(images)
-
-#         # Remove the dimension of size 1
-#         feature_vector = feature_vector.squeeze(0)
-#         # Convert tensor to bytes
-#         buffer = BytesIO()
-#         torch.save(feature_vector, buffer)
-#         feature_vector_bytes = buffer.getvalue()
-
-#         # You can get the filename from the path
-#         full_path = train_dataset.samples[idx][0]
-#         filename = os.path.basename(full_path)
-#         class_name = train_dataset.classes[labels[0]]
-
-#         # Insert a row of data via parameterized query to prevent SQL injection
-#         c.execute("INSERT INTO features (class, image_name, tensor) VALUES (?, ?, ?)", (class_name, filename, feature_vector_bytes))
-
-#         #print(f"Full path: {full_path}")
-#         #print(f"File name: {filename}")
-#         #print(f"Subfolder name: {class_name}")
-#         #print("-------------------")
-
-# # Replace 'root_folder' with the path to your root folder
-# get_file_info('/home/dsosatr/tesis/DYBtrainCropped/', transformada)
-
-# # Save (commit) the changes
-# conn.commit()
-
-# # Close the connection
-# conn.close()
-
-def store_features(val_loader, model):
-    """validation"""
+def process_image_batch(val_loader, model):
+    """
+    Processes a batch of images using a given model and stores the resulting feature vectors in an SQLite database.
+    Args:
+        val_loader (torch.utils.data.DataLoader): DataLoader providing batches of images and labels.
+        model (torch.nn.Module): The neural network model used to extract feature vectors from images.
+    Returns:
+        None
+    """
     model.eval()
+    
+    # Connect to SQLite database (or create it if it doesn't exist)
+    conn = sqlite3.connect('plankton_db.sqlite')
+    cursor = conn.cursor()
+    init_db(conn, cursor)
 
     with torch.no_grad():
-        for idx, (images, labels) in enumerate(val_loader):
+        for idx, (images, labels) in enumerate(tqdm(val_loader)):
             images = images.float().cuda()
             labels = labels
             bsz = len(labels)
 
             # forward
             image_feature_vector = model.encoder(images)
-            print("La ultima etiqueta es: ", labels[-1])
+            for label, feature_vector in zip(labels, image_feature_vector):
+                store_feature(label, feature_vector.cpu().numpy().flatten(), cursor, conn)
+
+    conn.close()
 
 def set_model(opt):
+    """
+    Initializes and sets up the model, classifier, and criterion for training or evaluation.
+
+    Args:
+        opt (Namespace): A namespace object containing the following attributes:
+            - model (str): The name of the model to be used.
+            - n_cls (int): The number of classes for the classifier.
+            - ckpt (str): The path to the checkpoint file.
+
+    Returns:
+        tuple: A tuple containing the following elements:
+            - model (torch.nn.Module): The initialized and loaded model.
+            - classifier (torch.nn.Module): The initialized classifier.
+            - criterion (torch.nn.Module): The loss function criterion.
+
+    Raises:
+        NotImplementedError: If no GPU is available.
+    """
+
     model = SupConResNet(name=opt.model)
     criterion = torch.nn.CrossEntropyLoss()
 
@@ -178,15 +129,46 @@ def set_model(opt):
 
     return model, classifier, criterion
 
-# Create a custom dataset to include class names
 class CustomDataset(datasets.ImageFolder):
+    """
+    A custom dataset class that extends the ImageFolder dataset from torchvision.
+
+    This class overrides the __getitem__ method to return the image tensor along with the class name
+    instead of the class index.
+
+    Methods
+    -------
+    __getitem__(index)
+        Returns the image tensor and the class name for the given index.
+
+    Parameters
+    ----------
+    index : int
+        Index of the sample to be fetched.
+
+    Returns
+    -------
+    tuple
+        A tuple containing the image tensor and the class name.
+    """
     def __getitem__(self, index):
         original_tuple = super(CustomDataset, self).__getitem__(index)
         path, _ = self.samples[index]
         class_name = self.classes[original_tuple[1]]
         return original_tuple[0], class_name
 
-def set_loader(opt):    
+def set_loader(opt):
+    """
+    Sets up the data loader for the validation dataset.
+
+    Args:
+        opt (Namespace): A namespace object containing the following attributes:
+            - dataset_path (str): Path to the dataset directory.
+            - batch_size (int): Number of samples per batch to load.
+
+    Returns:
+        DataLoader: A PyTorch DataLoader for the validation dataset.
+    """
     mean = (0.0418, 0.0353, 0.0409)
     std = (0.0956, 0.0911, 0.0769)
     normalize = transforms.Normalize(mean=mean, std=std)
@@ -210,6 +192,28 @@ def set_loader(opt):
     return val_loader
 
 class Options:
+    """
+    A class to represent configuration options for a CNN model.
+
+    Attributes
+    ----------
+    dataset_path : str
+        Path to the dataset used for training.
+    batch_size : int
+        Number of samples per batch.
+    model : str
+        Name of the model architecture to be used.
+    n_cls : int
+        Number of classes in the dataset.
+    ckpt : str
+        Path to the checkpoint file for the model.
+
+    Methods
+    -------
+    __init__():
+        Initializes the Options class with default values.
+    """
+
     def __init__(self):
         self.dataset_path = '/home/dsosatr/tesis/DYB-linearHead/train/'
         self.batch_size = 256
@@ -227,9 +231,10 @@ def main():
     # build model and criterion
     model, classifier, criterion = set_model(opt)
     
-    store_features(val_loader, model)
+    # process images and store features in database
+    process_image_batch(val_loader, model)
     
-    pass
+    print("Finished saving features of training set to database")
 
 
 if __name__ == '__main__':
