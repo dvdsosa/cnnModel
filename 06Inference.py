a8bacf6ea63631ad9e1ac02314896723cc1b3274
David Sosa-Trejo <dvdsosa@gmail.com>
2025-05-08 11:38:49 +0200

    feat: add timing and FAISS index size metrics in main
    Add timing measurements to track processing duration and calculate mean processing time per image. Include functionality to print the size of the FAISS index file in MB for better resource management.

--- 06Inference.py
+++ 06Inference.py
@@ -5,6 +5,8 @@ import numpy as np
 from torchvision import transforms, datasets
 from SupContrast.networks.resnet_big import SupConResNet
 from tqdm import tqdm
+import time
+import os
 import torch.backends.cudnn as cudnn
 from torcheval.metrics.functional import (
     multiclass_accuracy,
@@ -77,6 +79,7 @@ def main():
     idx_to_class = {v: k for k, v in class_to_idx.items()}
 
     model.eval()
+    start_time = time.time()  # Start timing
     with torch.no_grad():
         for images, real_labels in tqdm(val_loader, desc="Processing"):
             images = images.float().cuda()
@@ -98,6 +101,7 @@ def main():
             targets.append(class_to_idx[real_result])
 
     conn.close()
+    end_time = time.time()  # End timing
 
     # Compute metrics using torcheval
     preds_tensor = torch.tensor(preds)
@@ -113,6 +117,16 @@ def main():
     print(f"Multiclass Precision (macro): {prec.item() * 100:.2f}")
     print(f"Multiclass Recall (macro): {rec.item() * 100:.2f}")
     print(f"Multiclass F1 Score (macro): {f1.item() * 100:.2f}")
+    print(f"Total elapsed time: {end_time - start_time:.2f} seconds")
+    
+    # Calculate and print the mean processing time per image
+    total_images = len(val_loader.dataset)
+    mean_time_per_image = (end_time - start_time) / total_images
+    print(f"Mean processing time per image: {mean_time_per_image:.4f} seconds")
+
+    # Print the size of the FAISS index file
+    faiss_index_size = os.path.getsize(FAISS_INDEX_PATH) / (1024 * 1024)  # Convert bytes to MB
+    print(f"FAISS index file size: {faiss_index_size:.2f} MB")
 
 if __name__ == '__main__':
     main()
